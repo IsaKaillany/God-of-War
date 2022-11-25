5c9da3129636f2b7ca5135c22b6f348d7fb44436
Isa Kaillany <isakaillany123@gmail.com>
2022-11-24 21:07:24 -0300

    Aula 4

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@ var swiper = new Swiper(".slide-characters", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints: {
+        320: { //da resolução de 320px pra cima acontece algo
+            slidesPerView: 1.1,
+        },
+        768: {
+            slidesPerView: 2.2,
+        },
+        991: {
+            slidesPerView: 2.8,
+        },
+        1200: {
+            slidesPerView: 3.5,
+        }
+    }
 });
+
+AOS.init({
+    duration: 1000, //milisegundos
+    once: true,
+    
+})
